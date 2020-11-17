e16bbf6d9b8886b330e6b3a5e2ede10137b11289
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2020-11-17 23:50:57 +0000

    -Added composition provider to example.telemetry types -TODO:Find out why telemetry values only show up for tutorial openmct

--- src/plugins/simpleVuePlugin/plugin.js
+++ src/plugins/simpleVuePlugin/plugin.js
@@ -3,12 +3,14 @@ import HelloWorld from './HelloWorld.vue';
 
 
 function getDictionary() {
-	
 	function success()
 	{
-		let jsonfile = require('/home/vagrant/github/openmct/src/plugins/simpleVuePlugin/test.json');
+		//TODO: In the future will fix this path so it is not hard-coded
+		let jsonfile = require('/home/Lorenzo Gomez/openmct/src/plugins/simpleVuePlugin/test.json');
 		
-		console.log("success:", jsonfile);
+		let json  = "";
+		
+		// console.log("success:", jsonfile);
 	    return jsonfile;
 	}
 	
@@ -22,62 +24,108 @@ function getDictionary() {
 
 var objectProvider = {
 	   get: function (identifier) {
-       	console.log("identifier:", identifier);
-       	console.log("dictionary func:", getDictionary() )
+       	// console.log("identifier:", identifier);
+       	// console.log("dictionary func:", getDictionary() )
 	        return getDictionary().then(function (dictionary) {
-	        	console.log("identifier:", identifier);
+                // console.log("identifier:", identifier);
+                
+                // console.log("dictionary:", dictionary);
 	            if (identifier.key === 'spacecraft') {
-	            	console.log("match...");
+	            	// console.log("match...");
 	                return {
 	                    identifier: identifier,
 	                    name: dictionary.name,
 	                    type: 'folder',
 	                    location: 'ROOT'
 	                };
-	            }
+                }
+                
+                 else {
+                console.log("dictionary from ELSE:", dictionary);
+                        var measurement = dictionary.measurements.filter(function (m) {
+                        return m.key === identifier.key;
+                })[0];
+                return {
+                    identifier: identifier,
+                    name: measurement.name,
+                    type: 'example.telemetry',
+                    telemetry: {
+                        values: measurement.values
+                    },
+                    location: 'example.taxonomy:spacecraft'
+                };
+            }
 	        });
 	    }
 }
 
+var compositionProvider = {
+    appliesTo: function (domainObject) {
+        // console.log("comp provider domain object:", domainObject);
+        return domainObject.identifier.namespace === 'example.taxonomy' &&
+               domainObject.type === 'folder';
+    },
+    load: function (domainObject) {
+        return getDictionary()
+            .then(function (dictionary) {
+                return dictionary.measurements.map(function (m) {
+                    return {
+                        namespace: 'example.taxonomy',
+                        key: m.key
+                    };
+                });
+            });
+    }
+};
+
 function SimpleVuePlugin() {
     return function install(openmct) {
-        openmct.types.addType('hello-world', {
-            name: 'Hello World',
-            description: 'An introduction object',
-            creatable: true
-        });
-        openmct.objectViews.addProvider({
-            name: "demo-provider",
-            key: "hello-world",
-            cssClass: "icon-packet",
-            canView: function (d) {
-                return d.type === 'hello-world';
-            },
-            view: function (domainObject) {
-                var vm;
 
-                return {
-                    show: function (container) {
-                        vm = new Vue(HelloWorld);
-                        container.appendChild(vm.$mount().$el);
-                    },
-                    destroy: function (container) {
-                        vm.$destroy();
-                    }
-                };
-            }
-        });
-        
-    openmct.objects.addRoot({
+            openmct.objects.addRoot({
         namespace: 'example.taxonomy',
         key: 'spacecraft'
     });
+    openmct.types.addType('example.telemetry', {
+    name: 'Example Telemetry Point',
+    description: 'Example telemetry point from our happy tutorial.',
+    cssClass: 'icon-telemetry'
+});
     
-    openmct.objects.addProvider('example.taxonomy', objectProvider);
-    let jsonfile = require('/home/vagrant/github/openmct/src/plugins/simpleVuePlugin/test.json');
-//    console.log("process cwd", process.cwd());
-    console.log("json:", jsonfile);
+        // openmct.types.addType('hello-world', {
+        //     name: 'Hello World',
+        //     description: 'An introduction object',
+            
+        //     creatable: true
+        // });
+        // openmct.objectViews.addProvider({
+        //     name: "demo-provider",
+        //     key: "hello-world",
+        //     cssClass: "icon-packet",
+        //     canView: function (d) {
+        //         console.log("d param value:", d);
+
+        //         return d.type === 'example';
+        //     },
+        //     view: function (domainObject) {
+        //         var vm;
+
+        //         return {
+        //             show: function (container) {
+        //                 vm = new Vue(HelloWorld);
+        //                 console.log("vue obj:", vm);
+        //                 container.appendChild(vm.$mount().$el);
+        //             },
+        //             destroy: function (container) {
+        //                 vm.$destroy();
+        //             }
+        //         };
+        //     }
+        // });
         
+    openmct.objects.addProvider('example.taxonomy', objectProvider);
+
+    openmct.composition.addProvider(compositionProvider);
+
     };
 }
 
