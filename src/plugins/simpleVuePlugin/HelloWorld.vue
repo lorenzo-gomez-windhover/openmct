7ca3147eca2baa73bcf558d6c6ba117a49e7ba75
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2020-11-23 15:16:24 -0600

    -Added timestamp and value to Vue template

--- src/plugins/simpleVuePlugin/HelloWorld.vue
+++ src/plugins/simpleVuePlugin/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
 <div class="example">id: {{ tlm.id }}</div>
+<div class="example">timestamp: {{ tlm.timestamp }}</div>
+<div class="example">value: {{ tlm.value }}</div>
 </template>
 
 <script>
